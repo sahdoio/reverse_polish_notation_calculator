f50f3cce904c0c59a5eabf7c392fc1c81d5829f2
Lucas Sahdo <lucas@hintify.io>
2022-10-05 17:16:32 -0300

    index.php

--- index.php
+++ index.php
@@ -7,4 +7,6 @@ require_once __DIR__.'/bootstrap.php';
 $calculator = new ReversePolishNotationCalculator;
 $result = $calculator->calculate(["3", "5", "+", "7", "2", "-", "*"]);
 
+echo 'Final result: ';
 print_r($result);
+echo "\n";
